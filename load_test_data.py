876df01bf8f4d9ddb431958bc9f7ff9696452b2b
Your Name <you@example.com>
2024-04-03 20:52:19 +0300

    add product and order to load

--- load_test_data.py
+++ load_test_data.py
@@ -9,7 +9,7 @@ def execute_many_command(
     sql_command: str,
     data: list[tuple[str | int]],
 ) -> None:
-    """Выполнить команду c разлиными данными.
+    """Выполнить команду c  разлиными данными.
 
     Args:
     ----
@@ -30,11 +30,32 @@ VALUES (%s)
 """
 rack_insert_values = [("А",), ("Б",), ("Ж",)]
 
+order_insert = """
+INSERT INTO ordert(order_number, consumer_name)
+VALUES (%s, %s)
+"""
+order_insert_values = [(10, "A"), (11, "B"), (14, "C"), (15, "D")]
+
+product_insert = """
+INSERT INTO product(name, article)
+VALUES (%s, %s)
+"""
+product_insert_values = [
+    ("Ноутбук", 1),
+    ("Телевизор", 2),
+    ("Телефон", 3),
+    ("Системный блок", 4),
+    ("Часы", 5),
+    ("Микрофон", 6),
+]
+
 
 def load() -> None:
     """Основная функция модуля."""
     migrate()
     execute_many_command(rack_insert, rack_insert_values)
+    execute_many_command(order_insert, order_insert_values)
+    execute_many_command(product_insert, product_insert_values)
 
 
 if __name__ == "__main__":
